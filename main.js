1b85f16b217ed373ddc7ec8df4504c6f945af046
ronaldo <100880897+loriiian@users.noreply.github.com>
2024-05-16 00:11:27 -0300

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-      let roda = document.querySelector('.roda');
-      let botao = document.querySelector('.botao');
-      let valor = Math.ceil(Math.random() * 3600);
-
-      botao.onclick = function(){
-      roda.style.transform = "rotate(" + valor + "deg)";
-      valor += Math.ceil(Math.random() * 3600);
-      }
